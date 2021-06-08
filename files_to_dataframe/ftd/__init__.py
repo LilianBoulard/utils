26d97a6e8cf4105e104d00cd5c164c3239e03a3f
Lilian <lilian@boulard.fr>
2021-06-08 02:12:37 +0200

    Add new things to init

--- files_to_dataframe/ftd/__init__.py
+++ files_to_dataframe/ftd/__init__.py
@@ -1,6 +1,7 @@
 from .files_to_dataframe import FilesToDataFrame
 from .post_processing import PostProcessor
 
+from .analyzer import Analyzer
 from .dashboard import Dashboard
 
 from .utils import read_dataframe
--- files_to_dataframe/ftd/manipulators/__init__.py
+++ files_to_dataframe/ftd/manipulators/__init__.py
@@ -1,3 +1,4 @@
 from .by_extension import ByExtensionManipulator
 from .by_date import ByDateManipulator
 from .by_user import ByUserManipulator
+from .by_directory import ByDirectoryManipulator
