8d1f6ca1e146b93cbf46c71b6ec1f07269ec8dbb
Lilian <lilian@boulard.fr>
2021-06-08 02:11:05 +0200

    Add missing util

--- files_to_dataframe/ftd/utils.py
+++ files_to_dataframe/ftd/utils.py
@@ -2,8 +2,13 @@ import pandas as pd
 
 from time import time
 from pathlib import Path
+from typing import List
 
 from .config import parquet_write_kwargs, parquet_read_kwargs
+
+
+def get_lists_intersection(list1: List[int], list2: List[int]):
+    return list(set(list1) & set(list2))
 
 
 def log_duration(message):
@@ -21,9 +26,11 @@ def log_duration(message):
     return decorator
 
 
+@log_duration('Reading DataFrame')
 def read_dataframe(file_path: Path) -> pd.DataFrame:
     return pd.read_parquet(file_path, **parquet_read_kwargs)
 
 
+@log_duration('Writing DataFrame')
 def write_dataframe(file_path: Path, df: pd.DataFrame) -> None:
     df.to_parquet(file_path, **parquet_write_kwargs)
