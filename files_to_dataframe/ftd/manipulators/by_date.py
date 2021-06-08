d7697f2154cd7a9cff97fe41578e88818dbb0f1c
Lilian <lilian@boulard.fr>
2021-06-08 02:11:51 +0200

    Doc cleanup

--- files_to_dataframe/ftd/manipulators/by_date.py
+++ files_to_dataframe/ftd/manipulators/by_date.py
@@ -1,5 +1,7 @@
+import pandas as pd
+
 from time import time
-from typing import Dict, List
+from typing import Dict
 
 from .base import BaseManipulator
 from ..utils import log_duration
@@ -27,12 +29,29 @@ class ByDateManipulator(BaseManipulator):
     }
     """
 
-    ManipulatorContentType = Dict[str, Dict[str, List[int]]]
+    ManipulatorContentType = Dict[str, Dict[str, pd.Index]]
 
     LAST_ACCESS_COLUMN_NAME = 'atime'
     LAST_MODIFICATION_COLUMN_NAME = 'mtime'
     EXTENSION_COLUMN_NAME = 'extension'
 
+    # Date ranges
+    #
+    # How it works:
+    # Ranges must be ordered from the most recent to the oldest.
+    # For each entry, it must contain as key the name of the range,
+    # for instance "Less than 3 months", and as value the duration in seconds
+    # between the end of the preceding range and the end of this range
+    # (otherwise said, the duration that this range spans).
+    # Ranges are incremental, meaning the second range
+    # starts where the first range ends, and so on.
+    # The first range starts _now_ (the present moment).
+    # A special case is reserved for `0`, which indicated that the range
+    # spans all the way between the end of the preceding range,
+    # and January 1st, 1970 (Unix Epoch).
+    #
+    # If you decide to change these ranges, for them to take effect,
+    # you will need to recompute the stats.
     _ranges: Dict[str, int] = {
         'Less than 3 months': 60 * 60 * 24 * 30 * 3,
         '3 to 6 months': 60 * 60 * 24 * 30 * 3,
@@ -42,6 +61,7 @@ class ByDateManipulator(BaseManipulator):
     def sort(self) -> None:
         pass
 
+    @log_duration('Getting dates indices')
     def _compute(self) -> ManipulatorContentType:
         d = {
             'atime': {},
@@ -51,20 +71,17 @@ class ByDateManipulator(BaseManipulator):
         offset = int(time())
         for name, duration in self._ranges.items():
             t1 = offset
-            if duration == 0:
-                t0 = 0
-            else:
-                t0 = t1 - duration
+            t0 = 0 if duration == 0 else t1 - duration
 
             d['atime'].update({name: self._get_last_access_indices(t0, t1)})
-            # d['mtime'].update({name: self._get_last_modification_count_by_ext(t0, t1)})
+            # d['mtime'].update({name: self._get_last_modification_indices(t0, t1)})
 
             # Move offset
             offset = t0
 
         return d
 
-    def _get_indices_by_col(self, column_name: str, t0: int, t1: int) -> List[int]:
+    def _get_indices_by_col(self, column_name: str, t0: int, t1: int) -> pd.Index:
         """
         Queries a column to get the indices of files which timestamp
         in column `column_name` ranges between t0 (inclusive) and t1 (exclusive).
@@ -73,12 +90,10 @@ class ByDateManipulator(BaseManipulator):
         :param int t0: beginning UNIX timestamp
         :param int t1: end UNIX timestamp
         """
-        return list(self.df.query(f'{t0} <= {column_name} < {t1}').index)
+        return self.df.query(f'{t0} <= {column_name} < {t1}').index
 
-    @log_duration('Getting last access indices')
-    def _get_last_access_indices(self, t0: int, t1: int) -> List[int]:
+    def _get_last_access_indices(self, t0: int, t1: int) -> pd.Index:
         return self._get_indices_by_col(self.LAST_ACCESS_COLUMN_NAME, t0, t1)
 
-    @log_duration('Getting last modification indices')
-    def _get_last_access_indices(self, t0: int, t1: int) -> List[int]:
+    def _get_last_modification_indices(self, t0: int, t1: int) -> pd.Index:
         return self._get_indices_by_col(self.LAST_MODIFICATION_COLUMN_NAME, t0, t1)
